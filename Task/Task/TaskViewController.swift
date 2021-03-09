be56001d10e021ac5cadc4b9a6ffbf7ed5d9889d
Mark Poplawski <71023914+mpoplawski3@users.noreply.github.com>
2021-03-09 13:06:24 -0600

    Add files via upload

---
+++ Task/Task/TaskViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskViewController.swift
+//  Task
+//
+//  Created by Owner on 3/7/21.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var task: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        field.delegate = self
+
+        label.text = task
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
+    }
+    
+    @objc func deleteTask() {
+        
+
+    }
+   
+}
